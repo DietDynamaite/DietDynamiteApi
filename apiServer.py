f064e11a9075c8f110d2ad97696e1bca0f8f3147
최명식 <audtlr93@naver.com>
2024-09-12 15:57:55 +0900

    [Feat] 서버 전달 데이터 변경, 셀레니움 최적화

--- apiServer.py
+++ apiServer.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)   #CORS 오류 처리
 
-# TEST (동적 크롤링 일회성 클라이언트 반환용)
+# TEST URL 정보 (동적 크롤링 일회성 클라이언트 반환용)
 # http://192.168.10.16:7000/api/crawling/kakoImageOnce?mapId=26307587
 @app.route('/api/crawling/kakaoImageOnce')
 def kakoImageCrawlerOnce () :
@@ -23,13 +23,14 @@ def kakoImageCrawlerOnce () :
             result["src"] = "없음"
         return result
 
-# TEST (병렬처리 동적 크롤링 DB 저장용)
-# http://localhost:7000/api/crawling/kakaoImage?mapId=26307587
+# TEST URL 정보 (병렬처리 동적 크롤링 DB 저장용)
+# http://localhost:7000/api/crawling/kakaoImage?mapId=26307587&mapName=aaaaa
 @app.route('/api/crawling/kakaoImage')
 def kakoImageCrawler () :
     if (request.method == "GET"):
         mapId = request.args.get("mapId") #쿼리스트링
-        dynamicKakaoImageCrawling.delay(mapId)
+        mapName = request.args.get("mapName") #쿼리스트링
+        dynamicKakaoImageCrawling.delay(mapId, mapName)
     
     result = {
         "response" : "전달완료"
--- framework/crawler/kakaoMapImagesCrawler.py
+++ framework/crawler/kakaoMapImagesCrawler.py
@@ -37,74 +37,6 @@ def staticKakaoImageCrawling(mapId):
     else :
         return -1
 
-### 동적 이미지 크롤링 (DB 저장 용도)
-@celery_app.task(name="framework.crawler.kakaoMapImagesCrawler", queue='image_crawling_q', routing_key='default')
-def dynamicKakaoImageCrawling(mapId):
-    url = f"{IMAGE_BASE_URL}/{mapId}"
-    
-    # 페이지 로드 전략 None
-    caps = DesiredCapabilities.CHROME
-    caps["pageLoadStrategy"] = "none"
-            
-    # 크롬 드라이버 생성
-    chrome_options = ChromeOption()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--headless') 
-    chrome_options.add_argument('--ignore-ssl-errors=yes')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--log-level=3')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--incognito')
-
-    # Add Image Loading inactive Flag to reduce loading time
-    chrome_options.add_argument('--disable-images')
-    chrome_options.add_experimental_option(
-        "prefs", {'profile.managed_default_content_settings.images': 2})
-    chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.200'
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-    
-    # 페이지 접속
-    driver.get(url)
-    time.sleep(0.5)
-    
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    
-    imageEl = soup.select_one('a[data-viewid="basicInfoTopImage"]')
-    
-    if (imageEl != None):
-        #이미지 요소에서 style 속성 가져와 분해
-        imageStyle = imageEl.get("style")
-        start = imageStyle.index('url("//') + len('url("//')
-        end = imageStyle.index('");', start)
-        
-        imageSrc = imageStyle[start:end]
-        
-        # 이미지데이터 있을경우 placeImg 에 ImageSrc 를 가지고 Spring 에 요청 전송
-        requestData = {
-            "placeAPIid" : mapId,
-            "placeImg" : imageSrc
-        }
-        requestUrl = f"{SPRING_API_URL}/place/saveImage"
-        headers = {'Content-Type':'application/json; charset=utf-8'}
-        requests.post(url=requestUrl, data=json.dumps(requestData), headers=headers)
-        return
-    
-    # 이미지데이터 없을경우 placeImg 에 "0" 입력후 Spring 에 요청 전송
-    else :
-        requestData = {
-            "placeAPIid" : mapId,
-            "placeImg" : 0
-        }
-        requestUrl = f"{SPRING_API_URL}/place/saveImage"
-        headers = {'Content-Type':'application/json; charset=utf-8'}
-        requests.post(url=requestUrl, data=json.dumps(requestData), headers=headers)
-
-
 ### 동적 이미지 크롤링 (일회성 클라이언트에게 이미지 크롤링 용도)
 def dynamicKakaoImageCrawlingOnce(mapId):
     url = f"{IMAGE_BASE_URL}/{mapId}"
@@ -136,3 +68,75 @@ def dynamicKakaoImageCrawlingOnce(mapId):
     
     # 이미지 없으면 0 반환
     return "0"
+
+### 동적 이미지 크롤링 (DB 저장 용도)
+@celery_app.task(name="framework.crawler.kakaoMapImagesCrawler", queue='image_crawling_q', routing_key='default')
+def dynamicKakaoImageCrawling(mapId, mapName):
+    url = f"{IMAGE_BASE_URL}/{mapId}"
+    
+    # 페이지 로드 전략 None
+    caps = DesiredCapabilities.CHROME
+    caps["pageLoadStrategy"] = "none"
+            
+    # 크롬 드라이버 생성
+    chrome_options = ChromeOption()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--headless') 
+    chrome_options.add_argument('--ignore-ssl-errors=yes')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--disable-images')
+    chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.images': 2})
+    chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+
+    # Add Image Loading inactive Flag to reduce loading time
+    chrome_options.add_argument('--disable-images')
+    chrome_options.add_experimental_option(
+        "prefs", {'profile.managed_default_content_settings.images': 2})
+    chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.200'
+    chrome_options.add_argument(f'user-agent={user_agent}')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    
+    # 페이지 접속
+    driver.get(url)
+    time.sleep(0.5)
+    
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    
+    imageEl = soup.select_one('a[data-viewid="basicInfoTopImage"]')
+    
+    if (imageEl != None):
+        #이미지 요소에서 style 속성 가져와 분해
+        imageStyle = imageEl.get("style")
+        start = imageStyle.index('url("//') + len('url("//')
+        end = imageStyle.index('");', start)
+        
+        imageSrc = imageStyle[start:end]
+        
+        # 이미지데이터 있을경우 placeImg 에 ImageSrc 를 가지고 Spring 에 요청 전송
+        requestData = {
+            "placeAPIid" : mapId,
+            "placeImg" : imageSrc,
+            "placeName" : mapName
+        }
+        requestUrl = f"{SPRING_API_URL}/place/saveImage"
+        headers = {'Content-Type':'application/json; charset=utf-8'}
+        requests.post(url=requestUrl, data=json.dumps(requestData), headers=headers)
+        return
+    
+    # 이미지데이터 없을경우 placeImg 에 "0" 입력후 Spring 에 요청 전송
+    else :
+        requestData = {
+            "placeAPIid" : mapId,
+            "placeImg" : 0,
+            "placeName" : mapName
+        }
+        requestUrl = f"{SPRING_API_URL}/place/saveImage"
+        headers = {'Content-Type':'application/json; charset=utf-8'}
+        requests.post(url=requestUrl, data=json.dumps(requestData), headers=headers)
